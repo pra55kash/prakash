9112d9551ab02462efb5495b604d5fe8ed34f45b
pra55kash <pra55kash@gmail.com>
2018-12-22 13:37:18 +0530

    selenium 2

---
+++ Selenium-Maven/src/main/java/Selenium_Maven/Selenium_Maven/App.java
@@ -0,0 +1,13 @@
+package Selenium_Maven.Selenium_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
