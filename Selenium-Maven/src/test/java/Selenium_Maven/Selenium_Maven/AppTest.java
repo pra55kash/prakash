110b29f507e750ec9cd335bf3a8ca8a16f79dcb0
pra55kash <pra55kash@gmail.com>
2018-12-22 13:31:32 +0530

    Selenium project

---
+++ Selenium-Maven/src/test/java/Selenium_Maven/Selenium_Maven/AppTest.java
@@ -0,0 +1,24 @@
+package Selenium_Maven.Selenium_Maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+System.out.println("Yes Running");
+    	Assert.assertTrue( true );
+    }
+}
